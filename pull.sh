02e778ebd8d9c6be3c6f3727604cb878613ac7c2
Acil <acil.sutrisno@gmail.com>
2020-06-04 13:07:11 +0700

    tambah run script

--- pull.sh
+++ pull.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 cd /home/node/program_shiny/test_repo/ && git checkout script.R && chmod 777 -R /home/node/program_shiny/test_repo/*
+R CMD BATCH /home/node/program_shiny/test_repo/script.R
