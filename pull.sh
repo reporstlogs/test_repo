3fb72b167ba76583a11c6d3d28b6bed8234f19ef
Acil <acil.sutrisno@gmail.com>
2020-06-04 13:04:53 +0700

    tambah chmod 777

--- pull.sh
+++ pull.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-cd /home/node/program_shiny/test_repo/ && git checkout script.R
+cd /home/node/program_shiny/test_repo/ && git checkout script.R && chmod 777 -R /home/node/program_shiny/test_repo/*
